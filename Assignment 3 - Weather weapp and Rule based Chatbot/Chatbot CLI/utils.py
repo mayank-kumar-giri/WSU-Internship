7a454b7a622ea24b00fc1f0573f16359ff5c58d9
Mayank Kumar Giri <mayankgiri619@gmail.com>
2019-07-06 16:35:07 +0530

    Added Lemmatization before TFIDF calculation, Ask for elaboration if Q is too small

--- Assignment 3 - Weather weapp and Rule based Chatbot/Chatbot CLI/utils.py
+++ Assignment 3 - Weather weapp and Rule based Chatbot/Chatbot CLI/utils.py
@@ -3,6 +3,8 @@ import random
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
@@ -22,14 +24,14 @@ def LemNormalize(text):
 def read_corpus():
     fp = open('data.txt', "r+")
     raw = fp.read()
-    raw = raw.lower()
+    # raw = raw.lower()
     sent_tokens = raw.splitlines(True)
     # print(sent_tokens)
     ques_sent_tokens = []
     ans_sent_tokens = []
     for i in sent_tokens:
         ques, ans = i.split("-x-")
-        ques_sent_tokens.append(ques)
+        ques_sent_tokens.append(ques.lower())
         ans_sent_tokens.append(ans)
     return ques_sent_tokens,ans_sent_tokens
 
@@ -41,21 +43,45 @@ def write_corpus(ques_sent_tokens, ans_sent_tokens):
     fp.writelines(ques_sent_tokens)
     fp.close()
 
-def response(user_response,ques_sent_tokens,ans_sent_tokens):
-    user_response = nltk.sent_tokenize(user_response)
+def lemmatized_sentences(orig_ques_sent_tokens, orig_ans_sent_tokens):
+    ques_sent_tokens = []
+    ans_sent_tokens = []
+    lemmatizer = WordNetLemmatizer()
+    for i in range(len(orig_ques_sent_tokens)):
+        qsinit = word_tokenize(orig_ques_sent_tokens[i])
+        qsfinal = lemmatizer.lemmatize(qsinit[0])
+        for word in qsinit[1:]:
+            qsfinal += (" " + lemmatizer.lemmatize(word))
+        ansinit = word_tokenize(orig_ans_sent_tokens[i])
+        ansfinal = lemmatizer.lemmatize(ansinit[0])
+        for word in ansinit[1:]:
+            ansfinal += (" " + lemmatizer.lemmatize(word))
+        ques_sent_tokens.append(qsfinal)
+        ans_sent_tokens.append(ansfinal)
+    return ques_sent_tokens,ans_sent_tokens
+
+def response(user_response,ques_sent_tokens,orig_ans_sent_tokens):
+    lemmatizer = WordNetLemmatizer()
+    urinit = word_tokenize(user_response)
+    urfinal = lemmatizer.lemmatize(urinit[0])
+    for word in urinit[1:]:
+        urfinal += (" " + lemmatizer.lemmatize(word))
     chatty_response=''
     qst = list(ques_sent_tokens)
-    qst.append(user_response[0])
+    qst.append(urfinal)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize)
     tfidf = TfidfVec.fit_transform(qst)
     vals = cosine_similarity(tfidf[-1], tfidf)
     # print(vals)
     # print(vals.argsort())
-    # print(ans_sent_tokens)
+    # print(orig_ans_sent_tokens)
     idx=vals.argsort()[0][-2]
     if(vals[0][idx]<0.5):
-        chatty_response=chatty_response+"I am sorry! I don't understand you"
+        if len(urinit) <= 2:
+            chatty_response = chatty_response + "Can you please elaborate!"
+            return chatty_response, True
+        chatty_response = chatty_response+"I am sorry! I don't understand you"
         return chatty_response, False
     else:
-        chatty_response = chatty_response+ans_sent_tokens[idx]
+        chatty_response = chatty_response+orig_ans_sent_tokens[idx].strip()
         return chatty_response, True
--- Finals - To submit/Chatbot CLI/main.py
+++ Finals - To submit/Chatbot CLI/main.py
@@ -1,8 +1,11 @@
 import utils
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 if __name__== "__main__":
-    ques_sent_tokens, ans_sent_tokens = utils.read_corpus()
-    print("\nChatty: Hello there! This is Chatty!\n\t\tI'm a chatbot that answers questions related to\n\t\tAbraham Lincoln\n\n\t\tAsk me anything you'd like.\n\t\tEnter \"Exit\" when you wish to exit")
+    orig_ques_sent_tokens, orig_ans_sent_tokens = utils.read_corpus()
+    ques_sent_tokens, ans_sent_tokens = utils.lemmatized_sentences(orig_ques_sent_tokens, orig_ans_sent_tokens)
+    print("\nChatty: Hello there! This is Chatty!\n\tI'm a chatbot that answers questions related to\n\tAbraham Lincoln\n\n\tAsk me anything you'd like.\n\tEnter \"Exit\" when you wish to exit")
     while True:
         query = input()
         if query=="Exit":
@@ -12,15 +15,27 @@ if __name__== "__main__":
         if resp != "Normal":
             print("\nChatty:", resp.capitalize())
             continue
-        resp,valid = utils.response(query,ques_sent_tokens,ans_sent_tokens)
-        print("\nChatty:",resp.capitalize())
+        resp,valid = utils.response(query,ques_sent_tokens,orig_ans_sent_tokens)
+        print("\nChatty:",resp)
         if not valid:
-            print("\nChatty: Can you possibly tell me the answer for this query?\n\t\tIf yes, please tell the answer!\n\t\tIf no, just say No and then we can talk further :)")
+            print("\nChatty: Can you possibly tell me the answer for this query?\n\tIf yes, please tell the answer!\n\tIf no, just say No and then we can talk further :)")
             ans = input()
             if ans=="No":
-                print("\nChatty: No problem :)\n\t\tLet's continue talking!")
+                print("\nChatty: No problem :)\n\tLet's continue talking!")
             else:
-                ques_sent_tokens.append(query)
-                ans_sent_tokens.append(ans+"\n")
+                orig_ques_sent_tokens.append(query)
+                orig_ans_sent_tokens.append(ans+"\n")
+                lemmatizer = WordNetLemmatizer()
+                qsinit = word_tokenize(query)
+                qsfinal = lemmatizer.lemmatize(qsinit[0])
+                for word in qsinit[1:]:
+                    qsfinal += (" " + lemmatizer.lemmatize(word))
+                ansinit = word_tokenize(ans)
+                ansfinal = lemmatizer.lemmatize(ansinit[0])
+                for word in ansinit[1:]:
+                    ansfinal += (" " + lemmatizer.lemmatize(word))
+                ques_sent_tokens.append(qsfinal)
+                ans_sent_tokens.append(ansfinal+"\n")
+
                 print("\nChatty: Thanks a lot for sharing!")
-    utils.write_corpus(ques_sent_tokens,ans_sent_tokens)
+    utils.write_corpus(orig_ques_sent_tokens,orig_ans_sent_tokens)
