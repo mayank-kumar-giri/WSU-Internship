c57ce622b8dd0877c004501141e15cd398b4c59e
Mayank Kumar Giri <mayankgiri619@gmail.com>
2019-07-06 13:06:19 +0530

    CLI Chatbot - Optimized File operations - Reduced to single data File

---
+++ Assignment 3 - Weather weapp and Rule based Chatbot/Chatbot CLI/merge.py
@@ -0,0 +1,9 @@
+import nltk
+
+fp = open('data.txt',"r+")
+fp.seek(0,0)
+raw = fp.read().splitlines(True)
+for i in range(len(raw)):
+    print(raw[i].split("-x-"))
+# fp.writelines(final)
+fp.close()
